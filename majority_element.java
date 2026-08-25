b68f03fd8c37e4a7ccf4157f8e674fd279bcf9cf
RohitRawat16 <rohitrawat16j@gmail.com>
2026-08-25 23:24:01 +0530

    leetcode 169

---
+++ majority_element.java
@@ -0,0 +1,23 @@
+public class majority_element {
+    public static void main(String[] args) {
+        int[]arr = {2,2,1,1,1,2,2};
+        System.out.println(moore_voting(arr));
+    }
+    public static int moore_voting(int[]arr) {
+        int e =arr[0];
+        int vote = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]==e) {
+                vote++;
+            }
+            else{
+                vote--;
+                if (vote==0) {
+                  e= arr[i];
+                  vote = 1;                   
+                }
+            }
+        }
+        return e;
+    }
+}
